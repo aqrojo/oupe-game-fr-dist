5132764db630067e313073e3d25d8123362a71a6
aqrojo <aqrojo@oneclick.es>
2026-07-25 18:01:09 -0300

    ...

---
+++ assets/RibbonDemo-CO3Ben7y.js
@@ -0,0 +1,1 @@
+import{$ as e,E as t,R as n,w as r}from"./disclose-version-BFptNJba.js";import"./legacy-BDKuCDDI.js";import{o as i}from"./index-CncbZC04.js";var a=t(`<div class="container svelte-1lzua91"><!></div>`);function o(t){var o=a();i(n(o),{label:`Open response`}),e(o),r(t,o)}export{o as default};
